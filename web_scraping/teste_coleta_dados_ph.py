22c7c1f6d878f62c795349e9347968916c8da026
pedroribeiro003 <pedroribeiro003@hotmail.com>
2024-02-01 19:29:13 -0300

    update

--- web_scraping/teste_coleta_dados_ph.py
+++ web_scraping/teste_coleta_dados_ph.py
@@ -8,62 +8,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from selenium.webdriver.support import expected_conditions as EC
 
+# Define uma função para extrair dados de um imóvel
 def pegar_dados():
-
+    # Encontra elementos de texto e listas na página
     elements1 = driver.find_elements(By.CSS_SELECTOR, 'p')
     elements2 = driver.find_elements(By.CSS_SELECTOR, 'li')
-    '''
-    i = 0
-    for element in elements1:
-        print(f'{element.text}-{i}')
-        i+=1
-
-    #elements1
-    print(elements1[3].text)
-    print(elements1[5].text)
-    print(elements1[6].text)
-    print(elements1[9].text)
-    print(elements1[10].text)
-    print(elements1[12].text)
-    print(elements1[18].text)
-    #elements 2
-    print(elements2[2].text)
-    print(elements2[3].text)
-    print(elements2[4].text)
-    print(elements2[6].text)
-    print(elements2[7].text)
-    '''
-    #tratamento de dados
+    
+    # Realiza tratamento de dados para extrair informações relevantes
     price = float(''.join([caractere for caractere in elements1[3].text if caractere.isdigit()]))
     bedroom = int(''.join([caractere for caractere in elements1[5].text if caractere.isdigit()]))
     meters = ''.join([caractere for caractere in elements1[6].text if caractere.isdigit()])
     square_footage = float(meters[0:-1])
-    bathroom =int(''.join([caractere for caractere in elements1[10].text if caractere.isdigit()]))
+    bathroom = int(''.join([caractere for caractere in elements1[10].text if caractere.isdigit()]))
     realty_description = elements1[18].text
     city = elements2[2].text
     neighborhood = elements2[3].text
     street = elements2[4].text
     number = None
     condo = float(''.join([caractere for caractere in elements2[6].text if caractere.isdigit()]))
-    #property_tax = float(''.join([caractere for caractere in elements1[7].text if caractere.isdigit()]))
-
+    
     if elements1[9].text == '-':
         parking_space = 0
     else:
         parking_space = float(elements1.text)
-
+    
     if elements1[12].text == 'Sem mobília':
         realty_furnished = 0
-
     elif elements1[12].text == '':
         realty_furnished = None
-
     else:
         realty_furnished = 1
-
-    dict = {'street': street , 'square_footage': square_footage , 'price': price, 'description': realty_description, 'parking_space': parking_space , 'bathrooms': bathroom , 'bedroom': bedroom, 'property_tax': None, 'realty_furnished':realty_furnished , 'condo': condo }
-    print(dict)
-    return dict
+    
+    # Cria um dicionário com os dados extraídos
+    imovel_dict = {'street': street, 'square_footage': square_footage, 'price': price, 'description': realty_description, 'parking_space': parking_space, 'bathrooms': bathroom, 'bedroom': bedroom, 'property_tax': None, 'realty_furnished': realty_furnished, 'condo': condo}
+    
+    print(imovel_dict)
+    return imovel_dict
 
 # Configuração do WebDriver
 service = Service(ChromeDriverManager().install())
@@ -79,9 +59,10 @@ elements = driver.find_elements(By.TAG_NAME, 'h3')
 i = 0
 for element in elements:
     print(f'{element.text}-{i}')
-    i+=1
+    i += 1
 
-for i in range(0,11):
+# Loop para clicar em até 11 elementos e extrair dados de imóveis
+for i in range(0, 11):
     elements[i].click()
     data_imoveis.append(pegar_dados)
     driver.back
@@ -89,3 +70,4 @@ for i in range(0,11):
 print(data_imoveis)
 # Não esqueça de fechar o navegador depois de terminar
 driver.quit()
+
