fa52c4123aa0c31a7b490c3e6493be54a95697c3
Gabriel Brevilieri <123000180+gabrielbrev@users.noreply.github.com>
2024-02-07 01:21:36 -0300

    scraper principal zapimoveis finzalizado

--- realty_collection/zapimoveis_scraper.py
+++ realty_collection/zapimoveis_scraper.py
@@ -4,12 +4,18 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import threading
 from realty_scraper import scrap_realty
+import json
+import os
+from random import randint
 
 REALTY_DIV_SIZE = 300
 REALTIES_PER_PAGE = 100
 BASE_URL = "https://www.zapimoveis.com.br/venda/imoveis/rj+niteroi/?__ab=seo-texts:control,exp-aa-test:B&transacao=venda&onde=,Rio%20de%20Janeiro,Niterói,,,,,city,BR%3ERio%20de%20Janeiro%3ENULL%3ENiteroi,-22.823378,-43.04625,&pagina="
 
+pause_loading = False
+
 def load_realties(driver: webdriver.Chrome, realty_list_div: WebElement):
+    global pause_loading
     STEP = 100
     while True:
         loaded_realties = len(realty_list_div.find_elements(By.CLASS_NAME, "l-card__wrapper"))
@@ -20,17 +26,22 @@ def load_realties(driver: webdriver.Chrome, realty_list_div: WebElement):
 
         max_y = loaded_realties * 300
         current_scroll_y = driver.execute_script("return window.scrollY;")
-        if current_scroll_y == max_y:
+        if current_scroll_y >= max_y:
             driver.execute_script(f"window.scrollTo(0, {max_y * 0.2});")
         sleep(0.001)
 
-def scrape_website():
-    current_page = 1
-    scrapped_realties = 0
-    total_realties = 1
-    while scrapped_realties < total_realties:
+        while pause_loading:
+            sleep(0.3)
 
-        print(f"\nPÁGINA {current_page}")
+def scrape_website():
+    global pause_loading
+    all_realties = list()
+    current_page = 1
+    scraped_realties = 0
+    total_realties = 1
+    while scraped_realties < total_realties:
+
+        print(f"\nPAGE {current_page}")
 
         driver = webdriver.Chrome()
         driver.get(BASE_URL + f"{current_page}")
@@ -50,16 +61,25 @@ def scrape_website():
             try:
                 realty_div = driver.find_element(By.CSS_SELECTOR, f'div[data-position="{data_position}"]')
                 try:
-                    print(f"Scrapping realty number {data_position}")
+                    print(f"Scraping realty number {data_position}")
                     realty_a = realty_div.find_element(By.TAG_NAME, "a")
                     url = realty_a.get_attribute("href")
-                    print(scrap_realty(url))
-                except:
-                    print("Skipped (unsolved edge case)")
-                    show_all_button = realty_div.find_element(By.CSS_SELECTOR, 'button[data-testid="l-button"]')
+                except Exception as e:
+                    pause_loading = True
+                    show_all_button = realty_div.find_element(By.XPATH, ".//*[contains(text(), 'Exibir Anúncios')]")
+                    show_all_button.click()
+                    duplicate_list_div = driver.find_element(By.CLASS_NAME, "deduplication-listings__listings")
+                    duplicate_a_tags = duplicate_list_div.find_elements(By.TAG_NAME, "a")
+                    url = duplicate_a_tags[0].get_attribute("href")
+                    close_span = driver.find_element(By.CSS_SELECTOR, f'span[aria-label="Fechar modal lateral"]')
+                    close_span.click()
+                    pause_loading = False
                 finally:
+                    # realty_info = scrap_realty(url)
+                    # if realty_info != None:
+                    #     all_realties.append(realty_info)
                     data_position += 1
-                    scrapped_realties += 1
+                    scraped_realties += 1
             except:
                 print("Loading...", end="\r")
 
@@ -67,8 +87,16 @@ def scrape_website():
         driver.quit()
         current_page += 1
 
+    return all_realties
+
 def __main__():
-    scrape_website()
+    all_realties = scrape_website()
+    json_object = {"realties": all_realties}
+    print(f"JSON OBJECT {json_object}")
+    OUTPUT_PATH = r"realty_collection/output"
+    os.makedirs(OUTPUT_PATH)
+    with open(OUTPUT_PATH + f"/realties.json", "w") as json_file:
+        json.dump(json_object, json_file)
 
 if __name__ == "__main__":
     __main__()
