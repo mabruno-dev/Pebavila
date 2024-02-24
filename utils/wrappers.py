003217bd6cf60eaa2177fadef2c4e8a267ed6854
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-02-24 16:59:44 -0300

    improved announce decorator

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -23,47 +23,21 @@ from functools import wraps
 from io import StringIO
 import sys
 
-# def announce(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-        
-#         # Custom file-like class that writes to both stdout and a StringIO object
-#         class DualOutput:
-#             def __init__(self, stdout, captured):
-#                 self.stdout = stdout
-#                 self.captured = captured
-            
-#             def write(self, message):
-#                 self.stdout.write(message)
-#                 self.captured.write(message)
-            
-#             def flush(self):
-#                 # This is necessary for compatibility with the file interface
-#                 self.stdout.flush()
-#                 self.captured.flush()
-
-#         message = f"From {func.__name__}:"
-#         print(Console.BOLD_WHITE + message + Console.RESET, end=" ")
-
-#         captured_output = StringIO()
-#         dual_output = DualOutput(sys.stdout, captured_output)
-#         old_stdout = sys.stdout
-#         sys.stdout = dual_output
-
-#         result = func(*args, **kwargs)
-
-#         sys.stdout = old_stdout
-
-#         if not captured_output.getvalue():
-#             print("\r" + " " * len(message), end="\r")
-
-#         return result
-#     return wrapper
-
 def announce(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(Console.BLACK + f"Called {func.__name__}" + Console.RESET)
+
+        captured_output = StringIO()
+        old_stdout = sys.stdout
+        sys.stdout = captured_output
+
         result = func(*args, **kwargs)
+
+        sys.stdout = old_stdout
+        output = captured_output.getvalue()
+
+        if output:
+            print(Console.BLACK + f"From {func.__name__}: " + Console.RESET + output)
+
         return result
     return wrapper
