cc372a17888d746b62eff91777ed02186f3e1434
victorhugofpdev <victor.hugo.f.p.silva@gmail.com>
2024-02-27 04:09:29 -0300

    update

--- artificial_inteligence/data_analysis/teste.py
+++ artificial_inteligence/data_analysis/teste.py
@@ -1,34 +1,34 @@
 from gensim.models import KeyedVectors
+import gensim.downloader as api
+
+# Carregar seu modelo FastText
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import numpy as np
 import nltk
+import numpy as np
+
 from data_query import bring_description
+from googletrans import Translator, LANGUAGES
 
-# Baixe o conjunto de dados stopwords do NLTK, se ainda não o fez
-
-
-# Carregar um modelo pré-treinado (este é um exemplo, você precisará baixar um modelo real)
-# Para este exemplo, estamos usando um caminho fictício; você precisará substituí-lo pelo caminho do seu modelo pré-treinado
-# model = KeyedVectors.load_word2vec_format('path/to/your/model/word2vec/GoogleNews-vectors-negative300.bin', binary=True)
+translator = Translator()
 
 # Texto de descrição do imóvel
 texts = bring_description('realty_description', 'realties')
+# Modelo
+model = api.load('word2vec-google-news-300')
 # Pré-processamento
 for text in texts:
-    tokens = word_tokenize(text[0].lower())
-    stop_words = set(stopwords.words('portuguese'))
+    tokens = word_tokenize((translator.translate(text[0], src='pt', dest='en').text).lower())
+    stop_words = set(stopwords.words('english'))
     filtered_tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
 
-    # Extrair embeddings e calcular a média (simulação, já que não temos o modelo carregado)
-    # Para este exemplo, geraremos vetores aleatórios para simular os embeddings
+
     word_vectors = []
     for word in filtered_tokens:
         # Supondo que 'model' é o seu modelo Word2Vec carregado
-        # if word in model.vocab:
-        #     word_vectors.append(model[word])
-        # Simulação com vetores aleatórios
-        word_vectors.append(np.random.rand(300))  # 300 é uma dimensão comum para embeddings de palavras
+        if word in model.key_to_index:
+             word_vectors.append(model[word])
+        # Simulação com vetores aleatórios # 300 é uma dimensão comum para embeddings de palavras
 
     # Calcular a média dos vetores, se houver vetores disponíveis
     if word_vectors:
