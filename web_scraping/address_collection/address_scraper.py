6bd8675f14644540fe43e5d992e0bcd4afe4eeb1
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-03-20 21:27:07 -0300

    sync

--- web_scraping/address_collection/address_scraper.py
+++ web_scraping/address_collection/address_scraper.py
@@ -25,29 +25,32 @@ def __main__():
     for city_li in city_li_list:
         city_a = city_li.find_element(By.TAG_NAME, "a")
         driver.get(city_a.get_attribute("href"))
-        neighborhood_ul = driver.find_element(By.CLASS_NAME, "column-list")
-        neighborhood_li_list = neighborhood_ul.find_elements(By.TAG_NAME, "li")
+        try:
+            neighborhood_ul = driver.find_element(By.CLASS_NAME, "column-list")
+            neighborhood_li_list = neighborhood_ul.find_elements(By.TAG_NAME, "li")
 
-        for neighborhood_li in neighborhood_li_list:
-            neighbordhood_a = neighborhood_li.find_element(By.TAG_NAME, "a")
-            driver.get(neighbordhood_a.get_attribute("href"))
-            street_tr_list = find_table_rows(
-                driver.find_element(By.TAG_NAME, "table")
-            )
+            for neighborhood_li in neighborhood_li_list:
+                neighbordhood_a = neighborhood_li.find_element(By.TAG_NAME, "a")
+                driver.get(neighbordhood_a.get_attribute("href"))
+                street_tr_list = find_table_rows(
+                    driver.find_element(By.TAG_NAME, "table")
+                )
 
-            for street_tr in street_tr_list:
-                street_td_list = street_tr.find_elements(By.TAG_NAME, "td")
-                address = {
-                    "street": unidecode(street_td_list[1].find_element(By.TAG_NAME, "a").text).upper(),
-                    "neighborhood": unidecode(street_td_list[3].text.upper()),
-                    "city": unidecode(street_td_list[4].text.split("/")[0].upper()),
-                    "state": unidecode(street_td_list[4].text.split("/")[1].upper())
-                }
-                print(address)
-                all_addresses.append(address)
+                for street_tr in street_tr_list:
+                    street_td_list = street_tr.find_elements(By.TAG_NAME, "td")
+                    address = {
+                        "street": unidecode(street_td_list[1].find_element(By.TAG_NAME, "a").text).upper(),
+                        "neighborhood": unidecode(street_td_list[3].text.upper()),
+                        "city": unidecode(street_td_list[4].text.split("/")[0].upper()),
+                        "state": unidecode(street_td_list[4].text.split("/")[1].upper())
+                    }
+                    print(address)
+                    all_addresses.append(address)
 
-            driver.back()
-            sleep(5)
+                driver.back()
+                sleep(5)
+        except Exception as e:
+            print(f"Error in {city_li.text}")
 
         driver.back()
         sleep(5)
