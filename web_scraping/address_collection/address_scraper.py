5122a0bbe231447409ca270aafb5c31625d8b7b3
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-03-20 21:49:06 -0300

    sync

--- web_scraping/address_collection/address_scraper.py
+++ web_scraping/address_collection/address_scraper.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
 from time import sleep
 import json
 import os
@@ -11,29 +14,34 @@ def find_table_rows(table: WebElement):
     table_body = table.find_element(By.TAG_NAME, "tbody")
     return table_body.find_elements(By.TAG_NAME, "tr")
 
-
 def __main__():
     all_addresses = list()
 
-    driver = webdriver.Chrome()
-    driver.get("https://codigo-postal.org/pt-br/brasil/rio-de-janeiro/")
-    sleep(5)
+    driver = webdriver.Safari()
 
-    city_ul = driver.find_element(By.CLASS_NAME, "column-list")
+    driver.get("https://codigo-postal.org/pt-br/brasil/rio-de-janeiro/")
+
+    city_ul = wait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "column-list"))
+    )
     city_li_list = city_ul.find_elements(By.TAG_NAME, "li")
     
     for city_li in city_li_list:
         city_a = city_li.find_element(By.TAG_NAME, "a")
         driver.get(city_a.get_attribute("href"))
         try:
-            neighborhood_ul = driver.find_element(By.CLASS_NAME, "column-list")
+            neighborhood_ul = wait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "column-list"))
+            )
             neighborhood_li_list = neighborhood_ul.find_elements(By.TAG_NAME, "li")
 
             for neighborhood_li in neighborhood_li_list:
                 neighbordhood_a = neighborhood_li.find_element(By.TAG_NAME, "a")
                 driver.get(neighbordhood_a.get_attribute("href"))
                 street_tr_list = find_table_rows(
-                    driver.find_element(By.TAG_NAME, "table")
+                    wait(driver, 10).until(
+                        EC.presence_of_element_located((By.TAG_NAME, "table"))
+                    )
                 )
 
                 for street_tr in street_tr_list:
@@ -48,18 +56,16 @@ def __main__():
                     all_addresses.append(address)
 
                 driver.back()
-                sleep(5)
         except Exception as e:
             print(f"Error: {e}")
 
         driver.back()
-        sleep(5)
 
     driver.quit()
 
     # Transforma a lista de todos os endereços num JSON
     output_path = r"output"
-    os.makedirs(output_path)
+    os.makedirs(output_path, exist_ok=True)
     json_object = {"addresses": all_addresses}
     with open(output_path + r"/addresses.json", "w") as json_file:
         json.dump(json_object, json_file)
