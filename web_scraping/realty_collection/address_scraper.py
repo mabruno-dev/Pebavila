26d957d2851a09a3726dc53e61115fb5761e0ba5
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-04-20 18:44:12 -0300

    fix

--- web_scraping/realty_collection/address_scraper.py
+++ web_scraping/realty_collection/address_scraper.py
@@ -295,6 +295,7 @@ def __main__():
     producers = []
     for i in range(1):
         thread = threading.Thread(target=scrape_addresses, args=(0,  500 * i))
+        thread.daemon = True
         thread.start()
         producers.append(thread)
         sleep(1)
@@ -302,6 +303,7 @@ def __main__():
     consumers = []
     for i in range(5):
         thread = threading.Thread(target=scrape_realties, args=(630 + 30 * i, 30 * i))
+        thread.daemon = True
         thread.start()
         consumers.append(thread)
         sleep(1)
