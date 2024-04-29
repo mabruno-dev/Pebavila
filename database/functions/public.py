2c713b827ec177190d11bbd8f3779473e8c53e62
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-04-29 17:35:22 -0300

    added prints to location insertion function for better clarity

--- database/functions/public.py
+++ database/functions/public.py
@@ -165,7 +165,9 @@ def insert_neighborhood_city(database: Database, location: dict):
 def set_streets_neighborhoods_cities(json_streets):
     try:
         with Database() as database:
-            for street in json_streets['addresses']:
+            addresses = json_streets["addresses"]
+            for index, street in enumerate(addresses):
+                print(f"Inserting {index}/{len(addresses)} ({100 * index / len(addresses)})", end="\r")
                 insert_street_neighborhood_city(database, street)
     except Exception as e:
         db_error(database, e)
