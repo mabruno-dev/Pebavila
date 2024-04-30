8da499ec6ca3d850c17c40c5b1b2e3c81e8c8971
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-04-30 13:50:57 -0300

    fix?

--- web_scraping/address_collection/address_scraper.py
+++ web_scraping/address_collection/address_scraper.py
@@ -37,9 +37,10 @@ def write_json(all_addresses: dict):
     output_path = r"output"
     file_path = output_path + r"/addresses.json"
     
+    json_object = all_addresses.copy()
     os.makedirs(output_path, exist_ok=True)
     with open(file_path, "w") as json_file:
-        json.dump(all_addresses.copy(), json_file, ensure_ascii=False, indent=4)
+        json.dump(json_object, json_file, ensure_ascii=False, indent=4)
 
 def remove_deg(s: str):
     for i in range(1, 10):
